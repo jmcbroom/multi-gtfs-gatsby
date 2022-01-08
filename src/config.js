0bd5ff16094dbaa0f9ba342b442a8cd1b300eac5
Jimmy McBroom <james.mcbroom@gmail.com>
2022-01-08 14:28:34 -0500

    site-wide config docs

--- src/config.js
+++ src/config.js
@@ -1,3 +1,8 @@
+/**
+ * Site-wide configuration.
+ * 
+ * feedIndex maps the feedIndex numbers at the center of the database schema to a specific string we use in URL patterns
+ */
 const config = {
   feedIndexes: {
     8: `ddot`,
