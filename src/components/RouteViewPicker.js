5184bd29b09987b5fcf93d229e12aa716806da65
Jimmy McBroom <james.mcbroom@gmail.com>
2022-01-13 23:28:59 -0500

    move RouteViewPicker to own component

---
+++ src/components/RouteViewPicker.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { views } from "../templates/route-timetable-page";
+
+/**
+ * Let the user choose how they want to view the timetable.
+ * @param {*} view: current value from useState hook
+ * @param {*} setView: setter from useState hook
+ * @returns
+ */
+export const RouteViewPicker = ({ view, setView }) => {
+  return (
+    <div className="flex items-center justify-start w-full md:w-auto">
+      <span className="bg-gray-300 py-3 text-sm w-1/3 text-right px-4 md:text-center">
+        View
+      </span>
+      <select className="w-2/3 md:w-auto" value={view} onChange={(e) => setView(e.target.value)}>
+        {views.map(v => (
+          <option value={v} key={v}>{v}</option>
+        ))}
+      </select>
+    </div>
+  );
+};
