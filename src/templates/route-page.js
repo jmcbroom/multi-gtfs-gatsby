3f3618a358f8a9c15a284f10fc872338b4e52625
Jimmy McBroom <james.mcbroom@gmail.com>
2022-12-28 11:45:08 -0500

    more changes

--- src/templates/route-page.js
+++ src/templates/route-page.js
@@ -42,7 +42,9 @@ const Route = ({ data, pageContext }) => {
   let { serviceCalendars } = agencyData.feedInfo;
 
   let serviceDays = getServiceDays(serviceCalendars);
+  console.log(trips)
   let tripsByServiceDay = getTripsByServiceDay(trips, serviceDays);
+  console.log(trips)
   let headsignsByDirectionId = getHeadsignsByDirectionId(trips, sanityRoute);
   let tripsByServiceAndDirection = getTripsByServiceAndDirection(
     trips,
