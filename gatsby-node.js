7f6e68a2fea22bd87960a9a770a64e2cd1326496
Jimmy McBroom <james.mcbroom@gmail.com>
2022-12-28 11:45:08 -0500

    more changes

--- gatsby-node.js
+++ gatsby-node.js
@@ -65,7 +65,7 @@ exports.createPages = async ({ graphql, actions: { createPage } }) => {
             totalCount
           }
         }
-        stops: stopsList(filter: {feedIndex: {equalTo: ${a.currentFeedIndex}}}) {
+        stops: stopsList(filter: {feedIndex: {equalTo: ${a.currentFeedIndex}}}, first: 1000) {
           stopId
           stopCode
           feedIndex
