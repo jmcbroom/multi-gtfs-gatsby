023de830318b1ade3510b9d0e9c09e62d35939b1
Jimmy McBroom <james.mcbroom@gmail.com>
2023-01-08 13:31:15 -0500

    refactor RouteDirectionsTable into RouteIntroduction - closes #36: reformat `RouteDirectionsTable` on route-page.js to be human-readable paragraphs

--- src/components/RouteListItem.js
+++ src/components/RouteListItem.js
@@ -1,32 +1,37 @@
-import React from 'react';
-import {Link} from 'gatsby';
+import React from "react";
+import { Link } from "gatsby";
 
 /**
  * Displays the number badge and name of a route.
  * Pass a spread `route` GraphQL object with the following parameters:
- * @param {Number} feedIndex 
- * @param {String} shortName 
- * @param {String} longName 
- * @param {Number} routeColor 
- * @param {Number} routeTextColor 
+ * @param {Number} feedIndex
+ * @param {String} shortName
+ * @param {String} longName
+ * @param {Number} routeColor
+ * @param {Number} routeTextColor
  */
-const RouteListItem = ({ routeShortName, routeLongName, routeColor='#000', routeTextColor='#fff', agency }) => {
-
+const RouteListItem = ({
+  routeShortName,
+  routeLongName,
+  routeColor = "#000",
+  routeTextColor = "#fff",
+  agency,
+}) => {
   return (
     <Link to={`/${agency.slug.current}/route/${routeShortName}`}>
-      <li className="flex items-center justify-start gap-2 text-sm">
-        <span 
-          className="w-8 h-8 font-extrabold text-center justify-center items-center flex bg-white" 
-          style={{background: `${routeColor}`, color: `${routeTextColor}`}}
-        >
-          {routeShortName}
-        </span>
-        <span>
-          {routeLongName}
-        </span>
-      </li>
+      <div className="inline-block mr-2">
+        <div className="flex items-center justify-start gap-2 text-sm">
+          <span
+            className="w-8 h-8 font-extrabold text-center justify-center items-center flex bg-white"
+            style={{ background: `${routeColor}`, color: `${routeTextColor}` }}
+          >
+            {routeShortName}
+          </span>
+          <span className="text-base">{routeLongName}</span>
+        </div>
+      </div>
     </Link>
-  )
-}
+  );
+};
 
 export default RouteListItem;
