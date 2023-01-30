bdb7e074f24431df232ce857e2e109bad551c2ba
Jimmy McBroom <james.mcbroom@gmail.com>
2023-01-30 01:01:16 -0500

    add better GitHub link

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,3 +1,5 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { GitHubLogoIcon } from "@radix-ui/react-icons";
 import { Link } from "gatsby";
 import React from "react";
 import NavMenu from "./NavMenu";
@@ -26,8 +28,11 @@ export default function Layout({ children }) {
       </div>
       <footer className="h-32 mt-8 bg-primary px-2 md:px-4">
         <div className="grid grid-cols-1 md:grid-cols-2 max-w-4xl mx-auto">
-          <p>It's {new Date().getFullYear()}!</p>
-          <p>GitHub link</p>
+          <p>{new Date().getFullYear()}</p>
+          <div className="flex items-center justify-start gap-2">
+            <GitHubLogoIcon />
+            <p>GitHub: <a href="https://github.com/jmcbroom/multi-gtfs-gatsby">multi-gtfs-gatsby</a></p>
+          </div>
         </div>
       </footer>
     </div>
