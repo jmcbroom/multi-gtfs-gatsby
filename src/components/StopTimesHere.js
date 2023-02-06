643939fb72b6f1b945941bd27d08fffd7e4f4463
Jimmy McBroom <james.mcbroom@gmail.com>
2023-02-06 08:29:41 -0500

    changes - ServicePicker now day-aware - some styling consistency

--- src/components/StopTimesHere.js
+++ src/components/StopTimesHere.js
@@ -29,7 +29,13 @@ const StopTimesHere = ({ times, routes, agency, serviceDays }) => {
   let defaultRoute = routes.length > 0 ? routes[0].routeShortName : "none";
 
 
-  let defaultRoute = routes.length > 0 ? routes[0].routeShortName : 'none'
+  let [service, setService] = useState(defaultService);
+
+  
+  routes = routes.sort((a, b) => parseInt(a.routeShortName) > parseInt(b.routeShortName)).sort((a, b) => a.mapPriority > b.mapPriority);
+  let defaultRoute = routes.length > 0 ? routes[0].routeShortName : "none";
+
+  console.log(timesByRoute[routes[0].routeShortName][service]);
 
   return (
     <div>
