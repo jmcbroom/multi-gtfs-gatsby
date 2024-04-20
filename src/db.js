a8472addab913fbfd21dd4fe8ea4e25d35058976
Jimmy McBroom <james.mcbroom@gmail.com>
2024-04-20 08:30:51 -0400

    rename the IndexedDB database

--- src/db.js
+++ src/db.js
@@ -1,8 +1,9 @@
 // db.js
 import Dexie from 'dexie';
 
-export const db = new Dexie('myDatabase');
-db.version(3).stores({
+export const db = new Dexie('transit-det-city');
+
+db.version(4).stores({
   stops: '++id, agency, stop_id', // Primary key and indexed props
   bikeshare: '++id, agency, stop_id',
   routes: '++id, agency, route_id',
